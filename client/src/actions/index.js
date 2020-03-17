c5f207ccbf99dace1a460c63b481f2ec1b254e77
eugenekim000 <eugenekim000@gmail.com>
2020-03-16 20:17:32 -0700

    added reducers and rending simple li

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -22,14 +22,15 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post('./streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('./streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
 
 export const fetchStreams = () => async dispatch => {
-  const response = await streamsget('./streams');
+  const response = await streams.get('./streams');
 
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
